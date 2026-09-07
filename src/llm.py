4a802053bbb1c104ba3b81ca7161b2996d0b9797
Mohamed Bahri <simo@Mohameds-MacBook-Air.local>
2026-09-07 10:47:22 +0100

    Started llm.py

--- src/llm.py
+++ src/llm.py
@@ -1,17 +1,23 @@
+from typing import List
+from llm_sdk import Small_LLM_Model
 import time
+import numpy as np
+
 start = time.time()
 
-from llm_sdk import Small_LLM_Model
-from typing import List
+model = Small_LLM_Model()
 
 
-small = Small_LLM_Model()
+def generate_tokens(prompt: str, max_tokens: int) -> None:
+    raw_tensor = model.encode(prompt)
+    token_ids: List[int] = [int(x) for x in raw_tensor.squeeze(0).tolist()]
+    for _ in range(0, max_tokens):
+        raw_logits: list[float] = model.get_logits_from_input_ids(token_ids)
+        max_index: int = int(np.argmax(raw_logits))
+        token_ids.append(max_index)
 
-raw_tensor = small.encode("hello")
-token_ids: List[int] = [int(x) for x in raw_tensor]
-logits: list[float] = small.get_logits_from_input_ids(token_ids)
+    print(model.decode(token_ids))
 
 
-print(len(logits))
 end = time.time() - start
-print(f"{end:.2f}ms")
+print(f"{end:.2f} s")
